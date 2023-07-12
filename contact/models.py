119ee8489f3f6ad3cd4d50d485dc397434e47cd0
Matteus <mateus@ufscar.br>
2023-07-12 20:00:13 -0300

    criando primeiro models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+# Before
+# id(primary_key - autoincrement)
+# first_name(string), last_name(string), phone(string)
+# email(email), created_data(date), description(text)
+
+# After
+# category(forreing_key), show(boolean), owner(foreign_key)
+# picture(imagem)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100, null=False) 
+    last_name = models.CharField(max_length=100, null=False) 
+    phone = models.CharField(max_length=20) 
+    email = models.EmailField(max_length=254, blank=True)
+    created_data = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# inicialização do projeto Django
+
+python -m venv venv
+. venv/bin/activate
+pip install django
+django-admin startproject project .
+python manage.py stratapp contatc
+
+# Configurar o Git
+
+git config --global user.name 'Seu nome'
+git config --global user.email 'seu_email@gmail.com'
+git config --global init.defaultBranch main
+
+# Configure o .gitignore
+git init
+git add .
+git commit -m 'Mensagem'
+git remote add origin URL_DO_GIT
