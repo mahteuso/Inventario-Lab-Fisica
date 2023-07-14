02fe1e271f76b3ce0b2b84f1f9e217e55fa392e6
Matteus <mateus@ufscar.br>
2023-07-14 10:39:25 -0300

    novo commit

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Before
@@ -33,6 +34,11 @@ class Contact(models.Model):
     category = models.ForeignKey(
         Category, 
         on_delete=models.SET_NULL, 
+        blank=True, null=True
+        )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
         blank=True, null=True
         )
 
