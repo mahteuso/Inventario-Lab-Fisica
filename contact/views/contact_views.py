d59294b203759045ff0cd7a72cf4186464568737
Matteus <mateus@ufscar.br>
2023-08-04 16:58:22 -0300

    css

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -16,8 +16,7 @@ def index(request):
         context
         )
 def equipamento(request, contact_id):
-    single_contact = Contact.objects.get(pk=contact_id)
-
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     context = {
         'contact': single_contact
     }
